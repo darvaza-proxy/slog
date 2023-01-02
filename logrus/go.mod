a92714e4549c667b05f6bb18e45ee347ded7ff4f
Alejandro Mery <amery@jpi.io>
2023-01-02 22:39:19 +0000

    logrus: add initial NewWrapper()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- logrus/go.mod
+++ logrus/go.mod
@@ -4,4 +4,9 @@ go 1.19
 
 replace github.com/darvaza-proxy/slog => ../
 
-require github.com/darvaza-proxy/slog v0.0.0-00010101000000-000000000000
+require (
+	github.com/darvaza-proxy/slog v0.0.0-20230102194403-372027bb9066
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require golang.org/x/sys v0.3.0 // indirect
--- logrus/logrus.go
+++ logrus/logrus.go
@@ -1,11 +1,21 @@
 package logrus
 
 import (
+	"github.com/sirupsen/logrus"
+
 	"github.com/darvaza-proxy/slog"
 )
 
 var (
-	_ slog.Logger = (*StructuredLogger)(nil)
+	_ slog.Logger = (*LogrusLogger)(nil)
 )
 
-type StructuredLogger struct{}
+type LogrusLogger struct {
+	entry *logrus.Logger
+}
+
+func NewWrapper(entry *logrus.Logger) slog.Logger {
+	return &LogrusLogger{
+		entry: entry,
+	}
+}
