66d224a8f9df8e88e87b73791edcaba3b69e4b90
Alejandro Mery <amery@jpi.io>
2023-01-07 19:54:29 +0000

    slog: add initial methods to the Logger interface
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- slog.go
+++ slog.go
@@ -1,3 +1,13 @@
 package slog
 
-type Logger interface{}
+type Logger interface {
+	Debug(string, ...any)
+	Info(string, ...any)
+	Warn(string, ...any)
+	Error(string, ...any)
+	Fatal(string, ...any)
+
+	Printf(string, ...any)
+	WithField(string, any) Logger
+	WithFields(map[string]any) Logger
+}
