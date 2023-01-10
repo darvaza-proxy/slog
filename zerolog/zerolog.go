6093b51e6015b34019ea6cfc285ae33be0bc8e22
Alejandro Mery <amery@jpi.io>
2023-01-10 17:36:48 +0000

    zerolog: add skeleton to fullfill slog.Logger
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- zerolog/zerolog.go
+++ zerolog/zerolog.go
@@ -1,21 +1,88 @@
+// Package zerolog provides a slog.Logger adaptor using a github.com/rs/zerolog Logger as backend
 package zerolog
 
 import (
+	"fmt"
+
 	"github.com/rs/zerolog"
 
 	"github.com/darvaza-proxy/slog"
 )
 
 var (
-	_ slog.Logger = (*ZeroLogger)(nil)
+	_ slog.Logger = (*Logger)(nil)
 )
 
-type ZeroLogger struct {
+// Logger is an adaptor for using github.com/rs/zerolog as slog.Logger
+type Logger struct {
 	entry *zerolog.Logger
 }
 
+// Print adds a log entry with arguments handled in the manner of fmt.Print
+func (zl *Logger) Print(args ...any) {
+	zl.print(fmt.Sprint(args...))
+}
+
+// Println adds a log entry with arguments handled in the manner of fmt.Println
+func (zl *Logger) Println(args ...any) {
+	zl.print(fmt.Sprintln(args...))
+}
+
+// Printf adds a log entry with arguments handled in the manner of fmt.Printf
+func (zl *Logger) Printf(format string, args ...any) {
+	zl.print(fmt.Sprintf(format, args...))
+}
+
+func (zl *Logger) print(string) {}
+
+// Debug returns a new logger set to add entries as level Debug
+func (zl *Logger) Debug() slog.Logger {
+	return zl.WithLevel(slog.Debug)
+}
+
+// Info returns a new logger set to add entries as level Info
+func (zl *Logger) Info() slog.Logger {
+	return zl.WithLevel(slog.Info)
+}
+
+// Warn returns a new logger set to add entries as level Warn
+func (zl *Logger) Warn() slog.Logger {
+	return zl.WithLevel(slog.Warn)
+}
+
+// Error returns a new logger set to add entries as level Error
+func (zl *Logger) Error() slog.Logger {
+	return zl.WithLevel(slog.Error)
+}
+
+// Fatal returns a new logger set to add entries as level Fatal
+func (zl *Logger) Fatal() slog.Logger {
+	return zl.WithLevel(slog.Fatal)
+}
+
+// WithLevel returns a new logger set to add entries to the specified level
+func (zl *Logger) WithLevel(level slog.LogLevel) slog.Logger {
+	return zl
+}
+
+// WithStack attaches a call stack to a new logger
+func (zl *Logger) WithStack(skip int) slog.Logger {
+	return zl
+}
+
+// WithField returns a new logger with a field attached
+func (zl *Logger) WithField(label string, value any) slog.Logger {
+	return zl
+}
+
+// WithFields returns a new logger with a set of fields attached
+func (zl *Logger) WithFields(fields map[string]any) slog.Logger {
+	return zl
+}
+
+// NewWrapper creates a slog.Logger adaptor using a zerolog as backend
 func NewWrapper(entry *zerolog.Logger) slog.Logger {
-	return &ZeroLogger{
-		entry: logger,
+	return &Logger{
+		entry: entry,
 	}
 }
