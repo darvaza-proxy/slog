e8cad093ac03735401d8d3e70412215e788c3b1e
Alejandro Mery <amery@jpi.io>
2023-04-08 22:39:01 +0000

    build-sys: use `go list` instead of `go list -m`
    when go.work is enabled `go list -m` lists all modules
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- internal/tools/gen_index.sh
+++ internal/tools/gen_index.sh
@@ -7,7 +7,7 @@ set -eu
 MODULES=$(find * -name go.mod -exec dirname '{}' \;)
 GROUPS="x handlers"
 BASE="$PWD"
-MODULE=$($GO list -m)
+MODULE=$($GO list)
 
 namedir() {
 	local d="$1" g= n=
@@ -30,7 +30,7 @@ namedir() {
 
 mod() {
 	cd "$1"
-	$GO list -m
+	$GO list
 }
 
 mod_replace() {
